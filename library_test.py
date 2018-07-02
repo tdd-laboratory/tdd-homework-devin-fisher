6aedcec146690b9b9a83a1dfb95347ca038b3217
Devin Fisher <devin.fisher@evernym.com>
2018-07-02 15:49:04 -0600

    made several new tests pass and added a bunch of new test that don't yet pass

--- library_test.py
+++ library_test.py
@@ -30,6 +30,93 @@ class TestCase(unittest.TestCase):
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
 
+    def test_date_iso8601(self):
+        # The code seems designed to require a non-word character before and after the pattern
+        # because of this, the following test fails. Seemed out of scope fore exercise to fix this.
+        # self.assert_extract("2015-07-25", library.dates_iso8601, '2015-07-25')
+
+        self.assert_extract("I was born on 2015-07-25 in the state of Oregon", library.dates_iso8601, '2015-07-25')
+
+    def test_date_iso8601_invalid_month(self):
+        self.assert_extract("I was born on 2015-99-25.", library.dates_iso8601)
+        self.assert_extract("I was born on 2015-01-99.", library.dates_iso8601)
+
+    def test_date_noniso8601(self):
+        self.assert_extract("I was born 25 Jan 2017 in the state of Oregon", library.dates_non_iso8601, '25 Jan 2017')
+
+    def test_date_iso8601_precision_minutes_without_time_with_T(self):
+        self.assert_extract(" 2015-99-25T ", library.dates_iso8601, '2015-07-25')
+
+    def test_date_iso8601_precision_minutes_with_T(self):
+        self.assert_extract(" 2015-99-25T19:22 ", library.dates_iso8601, '2015-07-25T19:22')
+        self.assert_extract(" 2015-99-25T1922 ", library.dates_iso8601, '2015-07-25T1922')
+
+    def test_date_iso8601_precision_minutes_invalid_with_T(self):
+        self.assert_extract(" 2015-99-25T19:99 ", library.dates_iso8601)
+        self.assert_extract(" 2015-99-25T1999 ", library.dates_iso8601)
+
+    def test_date_iso8601_precision_minutes_without_T(self):
+        self.assert_extract(" 2015-99-25 19:22 ", library.dates_iso8601, '2015-07-25 19:22')
+        self.assert_extract(" 2015-99-25 1922 ", library.dates_iso8601, '2015-07-25 1922')
+
+    def test_date_iso8601_precision_minutes_invalid_without_T(self):
+        self.assert_extract(" 2015-99-25 19:99 ", library.dates_iso8601, '2015-99-25')
+        self.assert_extract(" 2015-99-25 1999 ", library.dates_iso8601, '2015-99-25')
+
+    def test_date_iso8601_precision_seconds_with_T(self):
+        self.assert_extract(" 2015-99-25T19:22:44 ", library.dates_iso8601, '2015-07-25T19:22:44')
+        self.assert_extract(" 2015-99-25T192244 ", library.dates_iso8601, '2015-07-25T1922')
+
+    def test_date_iso8601_precision_seconds_invalid_with_T(self):
+        self.assert_extract(" 2015-99-25T19:22:99 ", library.dates_iso8601)
+        self.assert_extract(" 2015-99-25T192299 ", library.dates_iso8601)
+
+    def test_date_iso8601_precision_seconds_without_T(self):
+        self.assert_extract(" 2015-99-25 19:22:44 ", library.dates_iso8601, '2015-07-25 19:22:44')
+        self.assert_extract(" 2015-99-25 192244 ", library.dates_iso8601, '2015-07-25 1922')
+
+    def test_date_iso8601_precision_seconds_invalid_without_T(self):
+        self.assert_extract(" 2015-99-25 19:22:99 ", library.dates_iso8601)
+        self.assert_extract(" 2015-99-25 192299 ", library.dates_iso8601)
+
+    def test_date_iso8601_precision_milliseconds_without_T(self):
+        self.assert_extract(" 2015-99-25 19:22:44.098 ", library.dates_iso8601, '2015-99-25 19:22:44.098')
+        self.assert_extract(" 2015-99-25 192244.908 ", library.dates_iso8601, '2015-99-25 192244.908')
+
+    def test_date_iso8601_precision_milliseconds_invalid_without_T(self):
+        self.assert_extract(" 2015-99-25 19:22:44.0=8 ", library.dates_iso8601)
+        self.assert_extract(" 2015-99-25 192244.9#8 ", library.dates_iso8601)
+
+    def test_date_iso8601_timezon_abbr(self):
+        self.assert_extract(" 2015-99-25T19:22:99MST ", library.dates_iso8601, '2015-99-25T19:22:99MST')
+        self.assert_extract(" 2015-99-25T192299MST ", library.dates_iso8601, '2015-99-25T192299MST')
+        self.assert_extract(" 2015-99-25 19:22:99MST ", library.dates_iso8601, '2015-99-25 19:22:99MST')
+        self.assert_extract(" 2015-99-25 192299MST ", library.dates_iso8601, '2015-99-25 192299MST')
+
+    def test_date_iso8601_timezon_Zulu(self):
+        self.assert_extract(" 2015-99-25T19:22:99Z ", library.dates_iso8601, '2015-99-25T19:22:99Z')
+        self.assert_extract(" 2015-99-25T192299Z ", library.dates_iso8601, '2015-99-25T192299Z')
+        self.assert_extract(" 2015-99-25 19:22:99Z ", library.dates_iso8601, '2015-99-25 19:22:99Z')
+        self.assert_extract(" 2015-99-25 192299Z ", library.dates_iso8601, '2015-99-25 192299Z')
+
+    def test_date_iso8601_timezon_offset(self):
+        self.assert_extract(" 2015-99-25T19:22:99+0100 ", library.dates_iso8601, '2015-99-25T19:22:99+0100')
+        self.assert_extract(" 2015-99-25T192299-0900 ", library.dates_iso8601, '2015-99-25T192299-0900')
+        self.assert_extract(" 2015-99-25 19:22:99-0800 ", library.dates_iso8601, '2015-99-25 19:22:99-0800')
+        self.assert_extract(" 2015-99-25 192299-01 ", library.dates_iso8601, '2015-99-25 192299-01')
+
+    def test_date_noniso8601_with_comma(self):
+        self.assert_extract("I was born 25 Jan, 2017 in the state of Oregon", library.dates_non_iso8601, '25 Jan, 2017')
+
+    def test_date_noniso8601_with_double_comma(self):
+        self.assert_extract("I was born 25 Jan,, 2017 in the state of Oregon", library.dates_non_iso8601)
+
+    def test_integers_comma_list(self):
+        self.assert_extract("The numbers on the wall were 123,456,789.", library.integers, '123', '456', '789')
+
+    def test_integers_comma_list_trailing_comma(self):
+        self.assert_extract("The numbers on the wall were 123,456,789,.", library.integers, '123', '456', '789')
+
 
 if __name__ == '__main__':
     unittest.main()
